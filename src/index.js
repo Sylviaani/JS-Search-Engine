56cf96092bca3b883ecf60d5df02d758f5586b4f
Sylviaani <sylviaani26@gmail.com>
2025-02-28 01:27:33 +0100

    parsed API date and updated href links

--- src/index.js
+++ src/index.js
@@ -1,35 +1,46 @@
 function displayTemperature(response) {
+  let cityElement = document.querySelector("#current-city");
+  cityElement.innerHTML = response.data.city;
+
   let temp = document.querySelector(".current-temperature-value");
   temp.innerHTML = Math.round(response.data.temperature.current);
 
-  let country = document.querySelector(".country");
-  country.innerHTML = response.data.country;
-
   let icon = document.querySelector(".current-temperature-icon");
-  icon.innerHTML = response.data.condition.icon;
+  icon.innerHTML = response.data.condition.icon_url;
 
   let description = document.querySelector("#current-desc");
   description.innerHTML = response.data.condition.description;
 
   let humidity = document.querySelector("#humid");
-  humidity.innerHTML = Math.round(response.data.temperature.humidity);
+  humidity.innerHTML = `${response.data.temperature.humidity}&`;
 
   let wind = document.querySelector("#windy");
-  wind.innerHTML = response.data.wind.speed;
+  wind.innerHTML = `${response.data.wind.speed}km/h`;
+
+  // parse API date
+  let time = document.querySelector("#current-date");
+  let date = new Date(response.data.time * 1000);
+
+  time.innerHTML = formatDate(date);
+
+  // let country = document.querySelector(".country");
+  // country.innerHTML = response.data.country;
+}
+
+function searchCity(city) {
+  let apiKey = "d3d1dee4f0701o614001t040202aba72";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=d3d1dee4f0701o614001t040202aba72&units=metric`;
+  axios.get(apiUrl).then(displayTemperature);
 }
 
 function search(event) {
   event.preventDefault();
-  let searchInputElement = document.querySelector("#search-input");
-  let cityElement = document.querySelector("#current-city");
+  let searchInput = document.querySelector("#search-input");
 
-  let apiKey = "d3d1dee4f0701o614001t040202aba72";
-  let city = searchInputElement.value;
-  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=d3d1dee4f0701o614001t040202aba72&units=metric`;
-  axios.get(apiUrl).then(displayTemperature);
-
-  cityElement.innerHTML = searchInputElement.value;
+  searchCity(searchInput.value);
 }
+
+searchCity("Lagos");
 
 function formatDate(date) {
   let minutes = date.getMinutes();
